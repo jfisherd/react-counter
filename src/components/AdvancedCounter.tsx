48ab043639d9fff2f6cbeec14c0200710c7e64cd
Josh Fisher <jfisherd@gmail.com>
2025-12-16 11:00:52 -0500

    styling improvement

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -8,45 +8,41 @@ export const AdvancedCounter: React.FC = (): ReactNode => {
     // UUID key for initial count history message generated with UUID V7 on https://www.uuidgenerator.net/
     const [countHistory, setCountHistory] = useState<ReactNode[]>([<li key={'019b2503-007d-74f3-a6f4-c19b6a898321'}>Count History:</li>])
 
-
-    // countHistory.map((countInstance) => <li>{countInstance}</li>)
+    // countHistory.map((countInstance) => <li>{countInstance}</li>) // Keep around for reference. -JF 12/16/2025
 
     const handleDecrement = () => {
-        let prevCountToBePassed = 0
-        setCount((prevCount) => {
-            prevCountToBePassed = prevCount
-            return prevCount - stepValue
-        })
         setCountHistory((prevCountHistory) => {
-            prevCountHistory.push(<li className="decrementRecord">({stepValue}) subtracted from Count ({prevCountToBePassed}) = {count}</li>); // keys not needed? Render OK. Console only noted keyless <li> elements on one occassion. JF 12/15/2025
+            prevCountHistory.push(<li className="decrementRecord">({stepValue}) subtracted from Count ({count}) = <strong>{count-stepValue}</strong></li>); // keys not needed? Render OK. Console only noted keyless <li> elements on one occassion. JF 12/15/2025
             return prevCountHistory
+        })
+        setCount((prevCount) => {
+            return prevCount - stepValue
         })
     }
 
     const handleIncrement = () => {
-        let prevCountToBePassed = 0
+        setCountHistory((prevCountHistory) => {
+            prevCountHistory.push(<li className="incrementRecord">({stepValue}) added to Count ({count}) = <strong>{count+stepValue}</strong></li>); // .push modifies an array then returns the length of the new array.
+            return prevCountHistory // The array must be returned, not the array length returned by .push()
+        })
         setCount((prevCount) => {
-            prevCountToBePassed = prevCount
             return prevCount + stepValue
         })
 
-        setCountHistory((prevCountHistory) => {
-            prevCountHistory.push(<li className="incrementRecord">({stepValue}) added to Count ({prevCountToBePassed}) = {count}</li>); // .push modifies an array then returns the length of the new array.
-            return prevCountHistory // The array must returned, not the array length
-        })
+
     }
 
     const handleStepValueChange = () => {
         setStepValue(Number(document.getElementById('stepInput').value)) // This somehow works despite the errors
-        {/* Number() required, type guards do not change input string to a number */ }
+        {/* Number() required, type guards do not change input string to a number? */ }
     }
 
     const handleReset = () => { // Clear count history and reset count to 0
         setCount(0)
-        setCountHistory([<li key={'019b2503-007d-74f3-a6f4-c19b6a898321'}>Reset button clicked</li>])
+        setCountHistory([<li key={'019b2503-007d-74f3-a6f4-c19b6a898321'}>Count History:</li>])
     }
 
-    // 
+    // FIX THE CSS COLORS TO NOT BE PAINFUL
 
 
     return (
@@ -60,7 +56,7 @@ export const AdvancedCounter: React.FC = (): ReactNode => {
                 <button onClick={handleIncrement}>
                     Increment
                 </button>
-                <button onClick={handleReset}>
+                <button id='buttonReset' onClick={handleReset}>
                     Reset Count
                 </button> {/* set current count to 0 */}
             </div>
