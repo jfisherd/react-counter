3412ff3fc0ea293885fb436718c8034920297242
Josh Fisher <jfisherd@gmail.com>
2025-12-15 21:28:28 -0500

    infinite loop resolved, default count history now displays

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -11,10 +11,9 @@ export const AdvancedCounter: React.FC = ():ReactNode => {
 
 
 
-    setListCountHistory(countHistory.map((countInstance) => <li>{countInstance}</li>))
+    // setListCountHistory(countHistory.map((countInstance) => <li>{countInstance}</li>)) // PUT THIS IN AN onChange SOMWHERE
     
-    {/* consider empty string case, division by 0, 0^0, undefined, bigInts, */ }
-    // const [customFunction, setCustomFunction] = useState<string>("")
+
 
 
     const handleStepValueChange = () => {
@@ -22,22 +21,22 @@ export const AdvancedCounter: React.FC = ():ReactNode => {
         {/* Number() required, type guards do not change input string to a number */ }
     }
 
-    // const handleIteration = () => {
-    //     // setCount(APPLY CUSTOM FUNCTION FROM INPUT DOM OBJECT)
-    // }
+
 
     const handleReset = () => {
         setCount(0)
         setCountHistory([<li>No count history</li>])
-        setListCountHistory([<li>No count history</li>])
-        {/*CLEAR COUNT HISTORY AND RESET COUNTER TO 0*/}
+            setListCountHistory(countHistory.map((countInstance) => <li>{countInstance}</li>))
+        // setListCountHistory([<li>No count history</li>])
+        // CLEAR COUNT HISTORY AND RESET COUNTER TO 0
+        // INFINITE LOOPS
     }
 
 
 
     return (
         <>
-            <h4>Advanced Counter + Iterator</h4> {/* <h4>Advanced Premium++ Gold Member VIP Honorary VP Counter</h4> */}
+            <h4>Advanced Counter</h4> 
             <h2>Current Count: {count}</h2>
             <div id='buttons'> {/* style={`display: flex; justify-content: center`}> NO IN-LINE STYLING?*/}
                 <button onClick={() => setCount((prevCount) => prevCount - stepValue)}>
@@ -46,20 +45,13 @@ export const AdvancedCounter: React.FC = ():ReactNode => {
                 <button onClick={() => setCount((prevCount) => prevCount + stepValue)}>
                     Increment
                 </button>
-                {/*<button onClick={handleIteration}>*/} {/* handle reducer function */}
-                    {/* Iterate f(x) */}
-                {/* </button>  */}
                 <button onClick={handleReset}> 
                     Reset Count
                 </button> {/* set current count to 0 */}
             </div>
             <label htmlFor="stepInput">Step size: </label> {/* Idea for label and stepInput id taken directly from Per Scholas' Lab 10.1 */}
             <input id="stepInput" type="number" value={stepValue} onChange={handleStepValueChange}></input> 
-            {/* <div id='reducerInputContainer'>
-                <label htmlFor='reducerInput'>f(x) = </label> */}
-                {/* <input id='reducerInput' type='text'></input> */} {/* Handle invalid inputs without breaking system */}
-                {/* <div> (e.g. x + x)</div>
-            </div> */}
+
             <div>!!! Changes Saved. !!!</div>
             <div>!!! Count History: !!!</div>
             <ul>{listCountHistory}</ul>
---
+++ src/components/NonsenseIgnoreMe.tsx
@@ -0,0 +1,30 @@
+
+
+////////////////////////////////////////////////////////////////////////
+/*  This file is used for testing elements. Keep for future testing   */
+////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+{/* consider empty string case, division by 0, 0^0, undefined, bigInts, */ }
+// const [customFunction, setCustomFunction] = useState<string>("")
+const handleIteration = () => {
+    // setCount(APPLY CUSTOM FUNCTION FROM INPUT DOM OBJECT)
+}
+
+
+<>
+    <h4>Advanced Premium++ Gold Member VIP Honorary VP Counter</h4>
+
+    <button onClick={handleIteration}> {/* handle reducer function */}
+        {/* Iterate f(x) */}
+    </button>
+
+    <div id='reducerInputContainer'>
+        <label htmlFor='reducerInput'>f(x) = </label>
+        <input id='reducerInput' type='text'></input>  {/* Handle invalid inputs without breaking system */}
+        <div> (e.g. x + x)</div>
+    </div>
+</>
