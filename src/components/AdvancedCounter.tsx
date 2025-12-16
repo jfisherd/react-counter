fbfdb226a74d22ff4f4704e75438bd3545c42744
Josh Fisher <jfisherd@gmail.com>
2025-12-15 22:46:42 -0500

    detailed count history updates/resets correctly, still double printing

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -6,15 +6,19 @@ export const AdvancedCounter: React.FC = (): ReactNode => {
     const [count, setCount] = useState<number>(0)
     const [stepValue, setStepValue] = useState<number>(1)
     // UUID key for initial count history message generated with UUID V7 on https://www.uuidgenerator.net/
-    const [countHistory, setCountHistory] = useState<ReactNode[]>([<li key={'019b2503-007d-74f3-a6f4-c19b6a898321'}>No count history</li>])
+    const [countHistory, setCountHistory] = useState<ReactNode[]>([<li key={'019b2503-007d-74f3-a6f4-c19b6a898321'}>Count History:</li>])
 
 
     // countHistory.map((countInstance) => <li>{countInstance}</li>)
 
     const handleDecrement = () => {
-        setCount((prevCount) => prevCount - stepValue)
+        let prevCountToBePassed = 0
+        setCount((prevCount) => {
+            prevCountToBePassed = prevCount
+            return prevCount - stepValue
+        })
         setCountHistory((prevCountHistory) => {
-            prevCountHistory.push(<li className="decrementRecord">({stepValue}) subtracted from Count ({ }) = { }</li>); // keys not needed? Render OK. Console only noted keyless <li> elements on one occassion. JF 12/15/2025
+            prevCountHistory.push(<li className="decrementRecord">({stepValue}) subtracted from Count ({prevCountToBePassed}) = {count}</li>); // keys not needed? Render OK. Console only noted keyless <li> elements on one occassion. JF 12/15/2025
             return prevCountHistory
         })
     }
@@ -22,11 +26,12 @@ export const AdvancedCounter: React.FC = (): ReactNode => {
     const handleIncrement = () => {
         let prevCountToBePassed = 0
         setCount((prevCount) => {
+            prevCountToBePassed = prevCount
             return prevCount + stepValue
         })
 
         setCountHistory((prevCountHistory) => {
-            prevCountHistory.push(<li className="incrementRecord">({stepValue}) added to Count ({ }) = { }</li>); // .push modifies an array then returns the length of the new array.
+            prevCountHistory.push(<li className="incrementRecord">({stepValue}) added to Count ({prevCountToBePassed}) = {count}</li>); // .push modifies an array then returns the length of the new array.
             return prevCountHistory // The array must returned, not the array length
         })
     }
@@ -38,7 +43,7 @@ export const AdvancedCounter: React.FC = (): ReactNode => {
 
     const handleReset = () => { // Clear count history and reset count to 0
         setCount(0)
-        setCountHistory([<li key={'019b2503-007d-74f3-a6f4-c19b6a898321'}>No count history</li>])
+        setCountHistory([<li key={'019b2503-007d-74f3-a6f4-c19b6a898321'}>Reset button clicked</li>])
     }
 
     // 
@@ -61,9 +66,7 @@ export const AdvancedCounter: React.FC = (): ReactNode => {
             </div>
             <label htmlFor="stepInput">Step size: </label> {/* Idea for label and stepInput id taken directly from Per Scholas' Lab 10.1 */}
             <input id="stepInput" type="number" value={stepValue} onChange={handleStepValueChange}></input>
-
             <div>!!! Changes Saved. !!!</div>
-            <div>!!! Count History: !!!</div>
             <ul>{countHistory}</ul>
         </>
     )
